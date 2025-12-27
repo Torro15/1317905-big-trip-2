e402a4ee42f208c5a51f69ae85c51e1567f02967
Светлана Г <svetlgrish28@gmail.com>
2025-12-27 20:54:04 +0300

    фильтр по умолчанию

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,43 +1,52 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createFilterItemTemplate (filter, currentFilterType) {
-  const {type, count} = filter;
+function createFilterTemplate(filterItems, currentFilterType) {
+  const filterItemsTemplate = filterItems
+    .map((filter) => createFilterItemTemplate(filter, currentFilterType))
+    .join('');
 
+  return `
+    <form class="trip-filters" action="#" method="get">
+      ${filterItemsTemplate}
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`;
+}
+
+function createFilterItemTemplate(filter, currentFilterType) {
+  const { type, count } = filter;
   const labelText = type.charAt(0).toUpperCase() + type.slice(1);
 
   const isChecked = type === currentFilterType ? 'checked' : '';
   const isDisabled = count === 0 ? 'disabled' : '';
+
   return `
-      <div class="trip-filters__filter">
-            <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${isChecked} ${isDisabled}>
-          <label class="trip-filters__filter-label" for="filter-${type}">${labelText}</label>
-        </div>
-              `;
+    <div class="trip-filters__filter">
+      <input
+        id="filter-${type}"
+        class="trip-filters__filter-input  visually-hidden"
+        type="radio"
+        name="trip-filter"
+        value="${type}"
+        ${isChecked}
+        ${isDisabled}>
+      <label class="trip-filters__filter-label" for="filter-${type}">
+        ${labelText}
+      </label>
+    </div>
+  `;
 }
-
-function createFilterTemplate(filterItems) {
-  const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
-    .join('');
-  return (
-    `<form class="trip-filters" action="#" method="get">
-        ${filterItemsTemplate}
-            </div><button class="visually-hidden" type="submit">Accept filter</button>
-              </form>`
-  );
-}
-
 
 export default class FiltersView extends AbstractView {
   #filters = null;
+  #currentFilterType = 'everything';
 
-  constructor({filters}) {
+  constructor({ filters, currentFilterType = 'everything' }) {
     super();
     this.#filters = filters;
+    this.#currentFilterType = currentFilterType;
   }
 
   get template() {
-    return createFilterTemplate(this.#filters);
+    return createFilterTemplate(this.#filters, this.#currentFilterType);
   }
-
 }
